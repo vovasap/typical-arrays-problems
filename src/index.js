03128f758b59e553348e183fadb72ea751578ef2
vovasap <vovasap.pblc@gmail.com>
2021-03-14 21:48:40 +0300

    refactor: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || !array.length) return 0
+    return Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || !array.length) return 0
+    return Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || !array.length) return 0
+    return array.reduce((el, sum)=> sum + el,0) / array.length
 }
